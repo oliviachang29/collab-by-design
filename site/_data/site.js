dcadf62f4232bf25ad63eee275f75e82a0fc0558
Olivia Chang <olivia.chang@limelighthealth.com>
2020-08-31 17:56:03 -0700

    add social image

--- site/_data/site.js
+++ site/_data/site.js
@@ -1,25 +1,14 @@
 module.exports = {
   name: "Knowledgy Lab",
-  shortDesc: "Leveraging family & community engagement learning",
+  shortDescription: "Leveraging family & community engagement learning",
+  longDescription: `"My name is Susana, and I write about how learning technologies and family STEM inclusion can help empower families to unlock their children's potential for learning."`,
   url: "https://knowledgylab.com",
   authorEmail: "hello@knowledgylab.com",
   authorName: "Susana Beltran",
   postsPerPage: 6,
   numPostsHomePage: 4,
   numExternalPostsHomePage: 2,
-  socialImage: "/img/social.jpg",
-  theme: {
-    primary: {
-      background: "white",
-      text: "black",
-      highlight: "#666",
-    },
-    secondary: {
-      background: "black",
-      text: "white",
-      highlight: "#666",
-    },
-  },
+  socialImage: "/uploads/socialImage.jpg",
   newsletter_title: "Subscribe to my newsletter",
   newsletter_subtitle:
     "Enter your email address to subscribe to this blog and receive notifications of new posts by email.",
